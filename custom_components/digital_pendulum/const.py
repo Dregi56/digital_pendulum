ba2a96901f07c30394458daa424794b9226b18b2
Dregi56 <79653938+Dregi56@users.noreply.github.com>
2026-02-13 17:28:52 +0100

    Update const.py

--- custom_components/digital_pendulum/const.py
+++ custom_components/digital_pendulum/const.py
@@ -10,6 +10,8 @@ CONF_PRESET_CHIME = "preset_chime"
 CONF_TOWER_CLOCK = "tower_clock"
 CONF_ANNOUNCE_HALF_HOURS = "announce_half_hours"
 CONF_VOICE_ANNOUNCEMENT = "voice_announcement"
+CONF_USE_VOLUME_CONTROL = "use_volume_control"
+CONF_ANNOUNCEMENT_VOLUME = "announcement_volume"
 
 # Defaults
 DEFAULT_ANNOUNCE_HALF_HOURS = True
@@ -21,6 +23,8 @@ DEFAULT_CUSTOM_CHIME_PATH = ""
 DEFAULT_PRESET_CHIME = "church-bell"
 DEFAULT_TOWER_CLOCK = False
 SWITCH_ENTITY_ID = "digital_pendulum_enabled"
+DEFAULT_USE_VOLUME_CONTROL = False
+DEFAULT_ANNOUNCEMENT_VOLUME = 0.5
 
 # Lista suoni predefiniti
 PRESET_CHIMES = {
@@ -77,3 +81,4 @@ PRESET_CHIMES = {
         "url": ""
     }
 }
+
